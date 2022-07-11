1810ee17d8169fd5986b8a026c8a42cb758c57dc
yoshi1203 <yoshivn1203@gmail.com>
2022-07-12 00:28:33 +0700

    tinh tong so duong

--- js/main.js
+++ js/main.js
@@ -3,5 +3,12 @@ let originalArray = [];
 themSo = () => {
   let inputNum = Number(document.getElementById('inputNum').value);
   originalArray.push(inputNum);
-  document.getElementById('txtArray').innerHTML = originalArray;
+  document.getElementById('originalArray').innerHTML = originalArray;
+};
+
+tinhTong = () => {
+  let sum = 0;
+  for (i = 0; i < originalArray.length; i++)
+    originalArray[i] > 0 && (sum += originalArray[i]);
+  document.getElementById('SumSoDuong').innerHTML = 'Tổng số dương: ' + sum;
 };
