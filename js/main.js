6d5f3a1a36c07d72dca1e140785d63e53e5e3914
yoshi1203 <yoshivn1203@gmail.com>
2022-07-12 00:52:14 +0700

    tim min

--- js/main.js
+++ js/main.js
@@ -10,12 +10,16 @@ tinhTong = () => {
   let sum = 0;
   for (i = 0; i < originalArray.length; i++)
     originalArray[i] > 0 ? (sum += originalArray[i]) : sum;
-  document.getElementById('SumSoDuong').innerHTML = 'Tổng số dương: ' + sum;
+  document.getElementById('SumSoDuong').innerHTML = `Tổng số dương: ${sum}`;
 };
 
 DemSoDuong = () => {
   let count = 0;
   for (i = 0; i < originalArray.length; i++)
     originalArray[i] > 0 ? count++ : count;
-  document.getElementById('DemSoDuong').innerHTML = 'Số dương: ' + count;
+  document.getElementById('DemSoDuong').innerHTML = `Số dương: ${count}`;
+};
+
+TimMin = () => {
+  document.getElementById('TimMin').innerHTML = Math.min(...originalArray);
 };
